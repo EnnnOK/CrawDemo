2777eab45726f4e0ee9769420a100deee6c2267a
Tsunamior <1092090655@qq.com>
2018-10-27 13:38:55 +0800

    startdemo

---
+++ crawUtil/craw.js
@@ -0,0 +1,2 @@
+module.paths = ['D:\\Software\\Software\\Node.js\\node_modules'];
+console.log("craw")
---
+++ databaseUtil/db.js
@@ -0,0 +1,35 @@
+module.paths = ['D:\\Software\\Software\\Node.js\\node_modules'];
+console.log(module.paths);
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "coin"
+
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("connect");
+        console.log(connection.state);
+        var d =Date.now().toLocaleString();
+        d = new Date();
+        console.log(d);
+        connection.query("insert into btc_t(id,date,price,chain_value,flow) values(?,?,?,?,?)",[1,d,1,1,1], function (err, res,fs) {
+            
+            if (err) {
+                console.log(err);
+                process.exit();
+            } else {
+                console.log(res);
+                process.exit();
+            }
+        });
+
+    }
+});
